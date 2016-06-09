b5fde2ef3cdcfabcf91a1baaf98a0a65a220966d
VickeeX <727951723@qq.com>
2016-06-09 13:52:20 +0800

    LoginActivity, not finished

--- app/src/main/java/com/vickee/wetalk/LoginActivity.java
+++ app/src/main/java/com/vickee/wetalk/LoginActivity.java
@@ -1,13 +1,15 @@
 package com.vickee.wetalk;
 
-import android.inputmethodservice.KeyboardView;
-import android.opengl.ETC1;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import com.netease.nimlib.sdk.RequestCallback;
 import com.netease.nimlib.sdk.auth.LoginInfo;
@@ -44,11 +46,16 @@ public class LoginActivity extends AppCompatActivity{
             @Override
             public void onSuccess(LoginInfo loginInfo) {
                 // the auto-login can be realized here
-            };
+
+                Intent intent = new Intent();
+                intent.setComponent(new ComponentName(LoginActivity.this, MainActivity.class));
+                startActivity(intent);
+                finish();
+            }
 
             @Override
-            public void onFailed(int i) {
-
+            public void onFailed(int code) {
+                Toast.makeText(LoginActivity.this, "登录失败" + code, Toast.LENGTH_SHORT).show();
             }
 
             @Override
@@ -57,7 +64,5 @@ public class LoginActivity extends AppCompatActivity{
             }
         };
 
-
     }
-
 }
