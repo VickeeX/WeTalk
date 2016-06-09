30d0d6610ee34d9250444cf0a5d98850e1c90339
VickeeX <727951723@qq.com>
2016-06-09 15:42:31 +0800

    LoginActivity: the initial view ok , some bug with login still.

--- app/src/main/java/com/vickee/wetalk/LoginActivity.java
+++ app/src/main/java/com/vickee/wetalk/LoginActivity.java
@@ -1,16 +1,15 @@
 package com.vickee.wetalk;
 
 import android.content.ComponentName;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.netease.nimlib.sdk.AbortableFuture;
 import com.netease.nimlib.sdk.NIMClient;
 import com.netease.nimlib.sdk.RequestCallback;
 import com.netease.nimlib.sdk.auth.AuthService;
@@ -21,51 +20,53 @@ import com.netease.nimlib.sdk.auth.LoginInfo;
  */
 public class LoginActivity extends AppCompatActivity{
 
-    final Button login_btn = (Button)findViewById(R.id.bt_login);
-    final Button register_btn = (Button)findViewById(R.id.bt_register);
-    final EditText account_et = (EditText)findViewById(R.id.et_account);
-    final EditText password_et = (EditText)findViewById(R.id.et_password);
+    private Button login_btn;
+    private Button register_btn;
+    private EditText account_et;
+    private EditText password_et;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
 
+        login_btn = (Button)findViewById(R.id.bt_login);
+        register_btn = (Button)findViewById(R.id.bt_register);
+        account_et = (EditText)findViewById(R.id.et_account);
+        password_et = (EditText)findViewById(R.id.et_password);
+
         login_btn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                login();
+                final String account = account_et.getText().toString().toLowerCase();
+                final String password = password_et.getText().toString().toLowerCase();
+
+                LoginInfo loginInfo = new LoginInfo(account,password);
+                RequestCallback<LoginInfo> callback = new RequestCallback<LoginInfo>() {
+                    @Override
+                    public void onSuccess(LoginInfo loginInfo) {
+                        // the auto-login can be realized here
+
+                        DemoCache.setAccount(account);
+                        Intent intent = new Intent();
+                        intent.setComponent(new ComponentName(LoginActivity.this, MainActivity.class));
+                        startActivity(intent);
+                        finish();
+                    }
+
+                    @Override
+                    public void onFailed(int code) {
+                        Toast.makeText(LoginActivity.this, "登录失败" + code, Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onException(Throwable throwable) {
+                        Toast.makeText(LoginActivity.this, "登录异常", Toast.LENGTH_SHORT).show();
+                    }
+                };
+                NIMClient.getService(AuthService.class).login(loginInfo).setCallback(callback);
             }
         });
     }
 
-    private void login(){
-        final String account = account_et.getEditableText().toString().toLowerCase();
-        final String password = password_et.getEditableText().toString().toLowerCase();
-
-        LoginInfo loginInfo = new LoginInfo(account,password);
-        RequestCallback<LoginInfo> callback = new RequestCallback<LoginInfo>() {
-            @Override
-            public void onSuccess(LoginInfo loginInfo) {
-                // the auto-login can be realized here
-
-                DemoCache.setAccount(account);
-                Intent intent = new Intent();
-                intent.setComponent(new ComponentName(LoginActivity.this, MainActivity.class));
-                startActivity(intent);
-                finish();
-            }
-
-            @Override
-            public void onFailed(int code) {
-                Toast.makeText(LoginActivity.this, "登录失败" + code, Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onException(Throwable throwable) {
-
-            }
-        };
-        NIMClient.getService(AuthService.class).login(loginInfo).setCallback(callback);
-    }
 }
