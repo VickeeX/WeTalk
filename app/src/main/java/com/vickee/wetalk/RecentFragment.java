bc693f25c5d297767a27784224eb78de9ffc4bbb
VickeeX <727951723@qq.com>
2016-06-15 15:28:25 +0800

    FriendsListAdapter, have the same bug as MyRecyclerAdapter, but can't be solved as the same way.

--- app/src/main/java/com/vickee/wetalk/RecentFragment.java
+++ app/src/main/java/com/vickee/wetalk/RecentFragment.java
@@ -1,16 +1,13 @@
 package com.vickee.wetalk;
 
-import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 
 import com.netease.nimlib.sdk.NIMClient;
 import com.netease.nimlib.sdk.RequestCallbackWrapper;
