e81d359d03a060c1f938297b788009d6455599ed
VickeeX <727951723@qq.com>
2016-06-17 20:43:11 +0800

    View: talk with another user.

---
+++ app/src/main/java/com/vickee/wetalk/TalkUserActivity.java
@@ -0,0 +1,13 @@
+package com.vickee.wetalk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TalkUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_talk_user);
+    }
+}
