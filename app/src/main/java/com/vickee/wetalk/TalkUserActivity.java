9cc0cfa40d705bbea915b726048d406f50715308
VickeeX <727951723@qq.com>
2016-06-30 15:21:26 +0800

    TalkUserActivity.

--- app/src/main/java/com/vickee/wetalk/TalkUserActivity.java
+++ app/src/main/java/com/vickee/wetalk/TalkUserActivity.java
@@ -2,15 +2,22 @@ package com.vickee.wetalk;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class TalkUserActivity extends AppCompatActivity {
 
-    private String content;
+    private EditText content_et;
+    private Button send_btn;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_talk_user);
+
+        content_et = (EditText)findViewById(R.id.msgText_et);
+        send_btn = (Button)findViewById(R.id.sendMsg_btn);
+
     }
 }
